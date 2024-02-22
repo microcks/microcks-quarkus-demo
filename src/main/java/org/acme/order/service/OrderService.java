893483881ee9cb7572b50d544aa6ba1e0a36deb8
Eric Deandrea <eric.deandrea@gmail.com>
2024-02-22 08:24:42 -0500

    Some additional cleanup

--- src/main/java/org/acme/order/service/OrderService.java
+++ src/main/java/org/acme/order/service/OrderService.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -27,6 +26,7 @@ import io.quarkus.logging.Log;
  */
 @ApplicationScoped
 public class OrderService {
+   private record PastryAvailability(String pastryName, boolean available) {}
 
    // This is a dumb implementation of an event sourcing repository. Don't use this in production!
    private final Map<String, List<OrderEvent>> orderEventsRepository = new HashMap<>();
@@ -45,38 +45,36 @@ public class OrderService {
     * @param info The order information.
     * @return A created Order with incoming info, new unique identifier and created status.
     * @throws UnavailablePastryException
-    * @throws Exception
     */
-   public Order placeOrder(OrderInfo info) throws UnavailablePastryException, Exception {
+   public Order placeOrder(OrderInfo info) throws UnavailablePastryException {
       // For all products in order, check the availability calling the Pastry API.
       var availabilityFutures = info.productQuantities().stream()
           .map(ProductQuantity::productName)
-          .collect(Collectors.toMap(this::checkPastryAvailability, Function.identity()));
+          .map(this::checkPastryAvailability)
+          .toList();
 
-      // Wait for all completable future to finish.
-      CompletableFuture.allOf(availabilityFutures.keySet().toArray(new CompletableFuture[0])).join();
+      // Wait for all completable futures to finish.
+      CompletableFuture.allOf(availabilityFutures.toArray(new CompletableFuture[availabilityFutures.size()])).join();
 
-      try {
-         // If one pastry is marked as unavailable, throw a business exception.
-         for (CompletableFuture<Boolean> availabilityFuture : availabilityFutures.keySet()) {
-            if (!availabilityFuture.get()) {
-               String pastryName = availabilityFutures.get(availabilityFuture);
-               throw new UnavailablePastryException(pastryName, "Pastry " + pastryName + " is not available");
-            }
-         }
-      } catch (InterruptedException | ExecutionException e) {
-         throw new Exception("Unexpected exception: " + e.getMessage());
+      var unavailablePastry = availabilityFutures.stream()
+          .map(this::getAvailability)
+          .filter(availability -> !availability.available())
+          .findAny();
+
+      if (unavailablePastry.isPresent()) {
+         var pastryName = unavailablePastry.get().pastryName();
+         throw new UnavailablePastryException(pastryName, "Pastry %s is not available".formatted(pastryName));
       }
 
       // Everything is available! Create (and probably persist ;-) a new order.
-      Order result = new Order();
+      var result = new Order();
       result.setCustomerId(info.customerId());
       result.setProductQuantities(info.productQuantities());
       //result.setProductQuantities(List.of(new ProductQuantity("Millefeuille", 1)));
       result.setTotalPrice(info.totalPrice());
 
       // Persist and publish creation event.
-      OrderEvent orderCreated = new OrderEvent(System.currentTimeMillis(), result, "Creation");
+      var orderCreated = new OrderEvent(System.currentTimeMillis(), result, "Creation");
       persistOrderEvent(orderCreated);
       eventPublisher.publishOrderCreated(orderCreated);
 
@@ -97,11 +95,8 @@ public class OrderService {
     * @return
     */
    public Order getOrder(String id) throws OrderNotFoundException {
-      List<OrderEvent> orderEvents = orderEventsRepository.get(id);
-      if (orderEvents != null) {
-         return orderEvents.get(orderEvents.size() - 1).order();
-      }
-      throw new OrderNotFoundException(id);
+      var orderEvents = getOrderEvents(id);
+      return orderEvents.get(orderEvents.size() - 1).order();
    }
 
    /**
@@ -111,30 +106,35 @@ public class OrderService {
     * @throws OrderNotFoundException
     */
    public List<OrderEvent> getOrderEvents(String id) throws OrderNotFoundException {
-      List<OrderEvent> orderEvents = orderEventsRepository.get(id);
-      if (orderEvents == null) {
-         throw new OrderNotFoundException(id);
-      }
-      return orderEvents;
+      return Optional.ofNullable(orderEventsRepository.get(id))
+          .orElseThrow(() -> new OrderNotFoundException(id));
    }
 
-   private CompletableFuture<Boolean> checkPastryAvailability(String pastryName) {
+   private CompletableFuture<PastryAvailability> checkPastryAvailability(String pastryName) {
       return CompletableFuture.supplyAsync(() -> {
              Log.infof("Checking pastry availability for pastry %s", pastryName);
-             return "available".equals(pastryRepository.getPastry(pastryName).status());
+             return new PastryAvailability(pastryName, "available".equals(pastryRepository.getPastry(pastryName).status()));
           })
           .exceptionally(e -> {
              Log.errorf(e, "Got exception from Pastry client: %s", e.getMessage());
-             return false;
+             return new PastryAvailability(pastryName, false);
           });
    }
 
    private void persistOrderEvent(OrderEvent event) {
-      List<OrderEvent> orderEvents = orderEventsRepository.get(event.order().getId());
-      if (orderEvents == null) {
-         orderEvents = new ArrayList<>();
-      }
+      var orderEvents = Optional.ofNullable(orderEventsRepository.get(event.order().getId()))
+          .orElseGet(ArrayList::new);
+
       orderEvents.add(event);
       orderEventsRepository.put(event.order().getId(), orderEvents);
+   }
+
+   private PastryAvailability getAvailability(CompletableFuture<PastryAvailability> availabilityFuture) {
+      try {
+         return availabilityFuture.get();
+      }
+      catch (InterruptedException | ExecutionException e) {
+         throw new RuntimeException("Unexpected exception: " + e.getMessage());
+      }
    }
 }
